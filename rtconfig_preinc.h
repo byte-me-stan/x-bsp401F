8b197bd2ac98c66cba379616f7349fb991586fc1
Stanley Lwin <we.byte.together@gmail.com>
2022-05-22 00:07:31 -0700

    Initial Import
    Initial import

---
+++ rtconfig_preinc.h
@@ -0,0 +1,19 @@
+
+#ifndef RTCONFIG_PREINC_H__
+#define RTCONFIG_PREINC_H__
+
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread pre-include file */
+
+#define ARDUINO 100
+#define ARDUINO_ARCH_RTTHREAD
+#define HAVE_CCONFIG_H
+#define RT_USING_LIBC
+#define RT_USING_NEWLIB
+#define STM32F401xE
+#define USE_HAL_DRIVER
+#define _POSIX_C_SOURCE 1
+#define __RTTHREAD__
+#define _REENT_SMALL
+
+#endif /*RTCONFIG_PREINC_H__*/
